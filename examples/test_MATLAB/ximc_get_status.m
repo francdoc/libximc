d481903fdefc2559b9bd616d382b0287d0e7169a
aryserg <aryserg3@gmail.com>
2021-11-16 17:27:01 +0300

    Bug #52853 Fix comments on matlab examples

--- examples/test_MATLAB/ximc_get_status.m
+++ examples/test_MATLAB/ximc_get_status.m
@@ -1,7 +1,10 @@
 function [ res_struct ] = ximc_get_status(device_id)
 
-% here is a trick.
-% we need to init a struct with any real field from the header.
+% To use the libximc library command get_status,
+% you need to initialize the status_t structure used by the commands.
+% Here is a trick.
+% It is necessary to initiate an arbitrary structure(dummy_struct) with a field that is present in the structure status_t.
+% Based on which the status_t structure should be set.
 dummy_struct = struct('Flags',999);
 parg_struct = libpointer('status_t', dummy_struct);
 [result, res_struct] = calllib('libximc','get_status', device_id, parg_struct);
--- examples/test_MATLAB/ximc_get_status_calb.m
+++ examples/test_MATLAB/ximc_get_status_calb.m
@@ -1,7 +1,10 @@
 function [ res_struct ] = ximc_get_status_calb(device_id, calibration)
 
-% here is a trick.
-% we need to init a struct with any real field from the header.
+% To use the libximc library command get_status_calb,
+% you need to initialize the status_calb_t structure used by the commands.
+% Here is a trick.
+% It is necessary to initiate an arbitrary structure(dummy_struct) with a field that is present in the structure status_calb_t.
+% Based on which the status_calb_t structure should be set.
 dummy_struct = struct('Flags',999);
 parg_struct = libpointer('status_calb_t', dummy_struct);
 parg2_struct = libpointer('calibration_t', calibration);
