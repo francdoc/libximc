50a12b522b642172c698984aaa601fbdc7c3de46
Anastasya Myasnikova <nikipov@gmail.com>
2022-05-25 13:47:47 +0300

    another posix warning fixed

--- libximc/src/udp-posix.c
+++ libximc/src/udp-posix.c
@@ -77,10 +77,6 @@ result_t open_udp(device_metadata_t *metadata, const char* ip4_port)
     metadata->handle = (uint32_t)socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if ((int)metadata->handle == -1) return result_error;
 
-    // setting  socket option
-    int OptVal = 1;
-    int OptLen = sizeof(int);
-
     struct timeval timeout;
     timeout.tv_sec = metadata->timeout / 1000;            // second part of timeout (which ordinary in milliseconds)
     timeout.tv_usec = (metadata->timeout % 1000) * 1000;  // millisecond part of timeout in microseconds
