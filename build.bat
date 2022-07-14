f3f1bd7049d004c83d3dfacd17ddb8e15fd14923
Anastasya Myasnikova <nikipov@gmail.com>
2022-07-14 19:23:31 +0300

    xibridge ebd.., build.bat with cygwin commented

--- build.bat
+++ build.bat
@@ -22,8 +22,9 @@
 @if "x%JDK_HOME%"=="x" goto FAIL
 
 :: read vers
-for /F %%i in (' c:\cygwin\bin\bash.exe --login -c "sed '3q;d' `cygpath '%BASEDIR%\version'`" ') do set XIBRIDGEVER=%%i
-if "%XIBRIDGEVER%VER%" == "" set XIBRIDGEVER=
+::for /F %%i in (' c:\cygwin\bin\bash.exe --login -c "sed '3q;d' `cygpath '%BASEDIR%\version'`" ') do set XIBRIDGEVER=%%i
+::if "%XIBRIDGEVER%VER%" == "" set XIBRIDGEVER=
+set XIBRIDGEVER=e6d0328
 echo Found xibridge ver %XIBRIDGEVER%
 
 :: debug flag
