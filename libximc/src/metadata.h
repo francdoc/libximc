33415c8b52613e7f028cd40ccabf3907bf37ce5f
Anastasya Myasnikova <nikipov@gmail.com>
2022-10-27 15:22:42 +0300

    metadata include path corrected

--- libximc/src/metadata.h
+++ libximc/src/metadata.h
@@ -5,7 +5,7 @@
  * Device metadata
  */
 #ifdef HAVE_XIBRIDGE
-  #include "xibridge.h"
+  #include "include/xibridge.h"
 #endif
 
 typedef enum { dtUnknown, dtSerial, dtVirtual, dtNet, dtUdp, dtTcp } device_type_t;
