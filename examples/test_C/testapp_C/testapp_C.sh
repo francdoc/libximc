7420b3e0c1e008861e3a78c1919d0ee8fbe4e7da
Anastasya Myasnikova <nikipov@gmail.com>
2022-12-15 11:47:11 +0300

    libximc ref pathes corrected

--- examples/test_C/testapp_C/testapp_C.sh
+++ examples/test_C/testapp_C/testapp_C.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WORKING_DIR=$(pwd) # Текущая директория
-LIBRARY_PATH=$WORKING_DIR/../../ximc
+LIBRARY_PATH=$WORKING_DIR/../../../ximc
 LINUX_ARCH=$(uname -m)
 echo Current architecture: $LINUX_ARCH
 case $LINUX_ARCH in
@@ -29,7 +29,7 @@ then
   echo "${FULL_LIBRARY_PATH}"
   export LD_LIBRARY_PATH=${FULL_LIBRARY_PATH}
   gcc testapp_C.c -L$FULL_LIBRARY_PATH -lximc -lbindy -lxiwrapper -I $LIBRARY_PATH -o testapp_C
-  #gcc testapp_C.c -L ../../ximc/debian-amd64 -lximc -lbindy -lxiwrapper -I ../../ximc -o testapp_C
-  #gcc testapp_C.c -lximc -lbindy -lxiwrapper -I ../../ximc -o testapp_C
+  #gcc testapp_C.c -L ../../../ximc/debian-amd64 -lximc -lbindy -lxiwrapper -I ../../ximc -o testapp_C
+  #gcc testapp_C.c -lximc -lbindy -lxiwrapper -I ../../../ximc -o testapp_C
   ./testapp_C  xi-com:/dev/ttyACM0
 fi
