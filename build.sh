8dea96e969f0608a25bb53c801b781702567e035
theirix <theirix@gmail.com>
2023-01-11 00:34:11 +0300

    Build miniupnpc from sources by CMake, #73080

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ XIWRAPPERVER=`sed '4q;d' "$VERSION_FILE"`
 if [ -z "$XIWRAPPERVER" ] ; then
 	XIWRAPPERVER=default
 fi
+MINIUPNPCVER=miniupnpd_2_3_0
 SOVERMAJOR=`echo $SOVER | sed 's/\..*//'`
 if [ -z "$SOVERMAJOR" ] ; then
 	echo Version error
@@ -444,12 +445,31 @@ build_dep_xiwrapper()
 	cp -a $DEPS/bindy/libbindy.* $DEPS/xiwrapper/
 }
 
+build_dep_miniupnpc()
+{
+	URL=https://github.com/transmission/miniupnpc
+
+	echo "--- Building miniupnpc ---"
+	if [ "x$SKIP_DEPS_CHECKOUT" != "xyes" ] ; then
+		rm -rf $DEPS/miniupnpc-dist $DEPS/miniupnpc
+		(cd $DEPS && git clone $URL miniupnpc-dist -b $MINIUPNPCVER)
+	fi
+	# cmake 3.5 is needed
+	(cd $DEPS/miniupnpc-dist && cmake $DEPS_CMAKE_OPT \
+		-DUPNPC_BUILD_TESTS=OFF -DUPNPC_BUILD_SAMPLE=OFF -DUPNPC_BUILD_SHARED=OFF \
+		-DCMAKE_INSTALL_PREFIX=$DEPS/miniupnpc $* .)
+	(cd $DEPS/miniupnpc-dist && cmake --build .)
+	(cd $DEPS/miniupnpc-dist && cmake --build . --target install)
+}
+
+
 build_depends()
 {
 	echo Building depends with flags $*
 	mkdir -p $DEPS
 	build_dep_bindy $*
 	build_dep_xiwrapper $*
+	build_dep_miniupnpc $*
 	echo Seed keyfile to libximc, seems like a hack
 	cp $DEPS/bindy/sample_keyfile.sqlite libximc/src/keyfile.sqlite
 	cp $DEPS/bindy/sample_keyfile.sqlite examples/test_C/testapp_C/keyfile.sqlite
