998860538602468703f3541eb481d9c501f267d1
aryserg <aryserg3@gmail.com>
2023-01-29 00:33:21 +0300

    Feature #74571 In the C examples, remove .pdb files.

--- build.bat
+++ build.bat
@@ -444,6 +444,9 @@ del examples\test_C\%NAME%\cb_obj /Q
 mkdir %DISTDIR%\win64\%NAME%-cb_compiled-win64
 copy examples\test_C\%NAME%\cb_compiled-win64\* %DISTDIR%\win64\%NAME%-cb_compiled-win64\*
 @if not %errorlevel% == 0 goto FAIL
+
+del %DISTDIR%\win32\%NAME%-compiled-win32\testapp_C.pdb
+del %DISTDIR%\win64\%NAME%-compiled-win64\testapp_C.pdb
 ::   clear env
 @SET PATH=%PATH_BASE%
 
@@ -486,6 +489,9 @@ del examples\test_C\%NAME%\cb_obj /Q
 mkdir %DISTDIR%\win64\%NAME%-cb_compiled-win64
 copy examples\test_C\%NAME%\cb_compiled-win64\* %DISTDIR%\win64\%NAME%-cb_compiled-win64\*
 @if not %errorlevel% == 0 goto FAIL
+
+del %DISTDIR%\win32\%NAME%-compiled-win32\testappeasy_C.pdb
+del %DISTDIR%\win64\%NAME%-compiled-win64\testappeasy_C.pdb
 ::   clear env
 @SET PATH=%PATH_BASE%
 
@@ -502,6 +508,9 @@ copy examples\test_C\%NAME%\compiled-win32\* %DISTDIR%\win32\%NAME%-compiled-win
 mkdir %DISTDIR%\win64\%NAME%-compiled-win64
 copy examples\test_C\%NAME%\compiled-win64\* %DISTDIR%\win64\%NAME%-compiled-win64\*
 @if not %errorlevel% == 0 goto FAIL
+
+del %DISTDIR%\win32\%NAME%-compiled-win32\testprofile_C.pdb
+del %DISTDIR%\win64\%NAME%-compiled-win64\testprofile_C.pdb
 
 :: -----
 @set NAME=test_CSharp
@@ -520,6 +529,9 @@ copy %DISTDIR%\win64\ximcnet.dll examples\%NAME%
 mkdir %DISTDIR%\win64\%NAME%-compiled-win64
 copy examples\%NAME%\compiled-win64\* %DISTDIR%\win64\%NAME%-compiled-win64\*
 @if not %errorlevel% == 0 goto FAIL
+
+del %DISTDIR%\win32\%NAME%-compiled-win32\test_CSharp.exe.config
+del %DISTDIR%\win64\%NAME%-compiled-win64\test_CSharp.pdb
 :: -----
 @set NAME=test_VBNET
 @echo Building example %NAME%...
