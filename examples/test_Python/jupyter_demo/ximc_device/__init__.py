b570cb3c191a8cb246eec2276189fa4bb8948e2c
timerke <timerke@mail.ru>
2023-02-01 10:28:07 +0300

    Added jupyter demo

---
+++ examples/test_Python/jupyter_demo/ximc_device/__init__.py
@@ -0,0 +1,6 @@
+from ximc_device.control_panel import ControlPanel
+from ximc_device.device import XimcDevice
+from ximc_device.open_panel import OpenPanel
+
+
+__all__ = ["ControlPanel", "OpenPanel", "XimcDevice"]
