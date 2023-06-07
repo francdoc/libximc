5729ea42f46d5acdef6e381ee739b1a5512832d8
Marladon <skyllann7@gmail.com>
2023-06-07 13:47:29 +0300

    del jupyter notebook

--- examples/test_Python/jupyter_demo/scripts/install_for_py_36_37.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-if exist venv rd /s/q venv
-set PYTHON=python
-%PYTHON% -m venv venv
-venv\Scripts\python -m pip install --upgrade pip
-venv\Scripts\python -m pip install -r scripts\requirements_for_py_36_37.txt
-pause
