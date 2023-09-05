264dfcc9cef59e0c09afee2907b56caade541e36
shampletov_no <shampletov_no@physlab.ru>
2023-09-05 13:03:18 +0300

    Revert "uri_path_to_absolute errs fixed"
    This reverts commit 2482a80ad76a969aafe76d80c580955cb624c4a6.

--- libximc/src/platform-posix.c
+++ libximc/src/platform-posix.c
@@ -761,22 +761,18 @@ void get_wallclock(time_t* sec, int* msec)
 	}
 }
 
-void uri_path_to_absolute(const char *uri_path, char *abs_path, size_t len)
+void uri_path_to_absolute(const char *uri_path, char *abs_path, size_t /*len*/)
 {
-    if (len < PATH_MAX)
-    {
-        *abs_path = 0;
+    /*
+    *abs_path = 0;
 
-        if (uri_path[0] != '/')
-            strncat(abs_path, "/", len);
-        strncat(abs_path, uri_path, len);
+    if (uri_path[0] != '/')
+    strncat(abs_path, "/", len);
+    strncat(abs_path, uri_path, len);
 
-        abs_path[len - 1] = 0;
-    }
-    else
-    {
-        realpath(uri_path, abs_path);
-    }
+    abs_path[len-1] = 0;
+    */
+    realpath(uri_path, abs_path);
 }
 
 /* Returns non-zero on success */
