9b2ca45baf49205effef9fe5745d0b485ecafd7f
shampletov_no <shampletov_no@physlab.ru>
2023-09-14 11:15:43 +0300

    Taking PyPI builder out of the repo

--- wrappers/python/pypi/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-python3 run.py
-if [ -d "./package" ]
-then
-    printf "\nBuilding distribution...\n"
-    cd "./package"
-    python3 -m pip install --upgrade build
-    python3 -m build
-fi
